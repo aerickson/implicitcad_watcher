479dec36245660ac11dfe3ea294eebda37c1355d
Andrew Erickson <aerickson@gmail.com>
2019-06-05 22:00:53 -0700

    openscad_watcher: work on .escad and .scad (#3)

--- lib/openscad_watcher.rb
+++ lib/openscad_watcher.rb
@@ -2,13 +2,13 @@
 
 require 'listen'
 
-# here now
 class OpenscadWatcher
   DEBUG_MODE = true
-  # TODO: take .scad and .escad
-  ESCAD_FILE_ENDING = '.scad'.freeze
-  ESCAD_FILE_GLOB = ('*' + ESCAD_FILE_ENDING).freeze
-  ESCAD_REGEX = /#{Regexp.escape(ESCAD_FILE_ENDING)}$/
+  # TODO: refactor names of these vars
+  # work on .scad and .escad
+  ESCAD_FILE_GLOB = [('*.scad'),
+                      '*.escad'].freeze
+  ESCAD_REGEX = /\.[e]*scad$/
   # TODO: allow cnofiguration or better detection for non OS X
   IMPLICITCAD_BIN = '/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD'.freeze
 
@@ -29,7 +29,8 @@ class OpenscadWatcher
 
   def self.get_result_file(source_file)
     dest_dir = File.dirname(source_file)
-    base_name = File.basename(source_file, ESCAD_FILE_ENDING)
+    base_name_with_suffix = File.basename(source_file)
+    base_name = base_name_with_suffix.split('.')[0]
     File.join(dest_dir, "#{base_name}.stl")
   end
 
